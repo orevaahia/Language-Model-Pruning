2321760fdea98f2ca36db124130a8c1139f4bbeb
Orevaoghene Ahia <oreva.ahia@gmail.com>
2019-12-06 02:34:44 +0100

    updated model script with LSTM model, added scrript to train pruned model

--- model.py
+++ model.py
@@ -3,7 +3,15 @@ import tensorflow as tf
 
 def GRU_model(vocab_size, embedding_dim, rnn_units, batch_size):
     """
-    
+    Function to build the GRU model 
+    Args:
+        vocab_size: Length of the vocabulary in chars
+        embedding_dim: The embedding dimension
+        rnn_units: Number of RNN units
+        batch_size: Batch size
+    Returns:
+        GRU model 
+
     """
     model = tf.keras.Sequential([
     tf.keras.layers.Embedding(vocab_size, embedding_dim,
@@ -19,3 +27,52 @@ def GRU_model(vocab_size, embedding_dim, rnn_units, batch_size):
      tf.keras.layers.Dense(vocab_size)
   ])
     return model
+
+def LSTM_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    """
+    Function that builds the  LSTM model 
+    Args:
+        vocab_size: Length of the vocabulary in chars
+        embedding_dim: The embedding dimension
+        rnn_units: Number of RNN units
+        batch_size: Batch size
+    Returns:
+        LSTM model 
+    """
+    model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.LSTM(rnn_units,
+                        return_sequences=True,
+                        stateful=True),
+    tf.keras.layers.Dense(vocab_size)
+   ])
+
+def print_model_sparsity(pruned_model):
+  """Prints sparsity for the pruned layers in the model.
+  Model Sparsity Summary
+  --
+  prune_lstm_1: (kernel, 0.5), (recurrent_kernel, 0.6)
+  prune_dense_1: (kernel, 0.5)
+  Args:
+    pruned_model: keras model to summarize.
+  Returns:
+    None
+  """
+  def _get_sparsity(weights):
+    return 1.0 - np.count_nonzero(weights) / float(weights.size)
+
+  print("Model Sparsity Summary ({})".format(pruned_model.name))
+  print("--")
+  for layer in pruned_model.layers:
+    if isinstance(layer, pruning_wrapper.PruneLowMagnitude):
+      prunable_weights = layer.layer.get_prunable_weights()
+      if prunable_weights:
+        print("{}: {}".format(
+            layer.name, ", ".join([
+                "({}, {})".format(weight.name,
+                                  str(_get_sparsity(K.get_value(weight))))
+                for weight in prunable_weights
+            ])))
+  print("\n")
+
