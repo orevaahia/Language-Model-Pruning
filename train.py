b98b339570c6bd5e9ffb8bce89623a2fc8be4ea5
Orevaoghene Ahia <oreva.ahia@gmail.com>
2019-12-27 19:04:46 +0100

    Modified model parameters

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-
+import tensorboard
 import numpy as np
 import os
 import sys
@@ -16,24 +16,25 @@ from model import GRU_model, LSTM_model
 # ==================================================
 
 # Data loading params
-tf.flags.DEFINE_string("dataset", "Data/republic_clean.txt", "Data file for the task")
+tf.app.flags.DEFINE_string("dataset", "Data/republic_clean.txt", "Data file for the task")
 
 # Model Hyperparameters
-tf.flags.DEFINE_integer("embedding_dim",512,"Dimensionality of character embedding (default: 128)")
-tf.flags.DEFINE_integer("RNN_units",1024,"Dimensionality of character embedding (default: 128)")
+tf.app.flags.DEFINE_integer("embedding_dim",512,"Dimensionality of character embedding (default: 128)")
+tf.app.flags.DEFINE_integer("RNN_units",1024,"Dimensionality of character embedding (default: 128)")
 
 # Training parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 40, "Number of training epochs ")
-tf.flags.DEFINE_integer("buffer_size", 1000, "Buffer size for shuffling ")
+tf.app.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
+tf.app.flags.DEFINE_integer("num_epochs", 40, "Number of training epochs ")
+tf.app.flags.DEFINE_integer("buffer_size", 1000, "Buffer size for shuffling ")
+tf.app.flags.DEFINE_boolean("drop_remainder", True, "Drop last batch if it has fewer batch size elements")
 
 
-FLAGS = tf.flags.FLAGS
+FLAGS = tf.app.flags.FLAGS
 FLAGS(sys.argv)
 
 def preprocess():
     """
-    
+    Prepare data 
     """
     data = FLAGS.dataset
     text = load_text(data)
@@ -52,21 +53,22 @@ def preprocess():
     char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
 
     # The batch method lets us easily convert these individual characters to sequences of the desired size.
-    sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+    sequences = char_dataset.batch(seq_length+1, drop_remainder=FLAGS.drop_remainder)
 
     # For each sequence, duplicate and shift it to form the input and target text by using the map method to apply a simple function to each batch:
     dataset = sequences.map(split_input_target)
 
     # shuffle the data and pack it into batches.
-    dataset = dataset.shuffle(FLAGS.buffer_size).batch(FLAGS.batch_size, drop_remainder=True)
+    dataset = dataset.shuffle(FLAGS.buffer_size).batch(FLAGS.batch_size, drop_remainder=FLAGS.drop_remainder)
     
     return vocab, dataset
 
-def train(dataset, model, vocab):
+
+def train(model, dataset, vocab):
     # Training
     # ==================================================
     
-    model = model(
+    unpruned_model = model(
         vocab_size = len(vocab),
         embedding_dim=FLAGS.embedding_dim,
         rnn_units=FLAGS.RNN_units,
@@ -74,20 +76,22 @@ def train(dataset, model, vocab):
 
     logdir = tempfile.mkdtemp()
     callbacks = [tf.keras.callbacks.TensorBoard(log_dir=logdir, profile_batch=0)]
-    model.compile(optimizer='adam', loss=loss)
-
-    history = model.fit(dataset, epochs = FLAGS.num_epochs, callbacks= callbacks)
+  
+    unpruned_model.compile(optimizer='adam', loss=loss)
+    unpruned_model.fit(dataset, 
+                       epochs = FLAGS.num_epochs, 
+                       callbacks= callbacks)
     
-    # Save the original model for size comparison later
+    # Save the  model 
 
-    _ , keras_file = tempfile.mkstemp('.h5')
+    _ , keras_file = tempfile.mkstemp('_unpruned.h5', dir = 'models/')
     print('Saving model to: ', keras_file)
-    tf.keras.models.save_model(model, keras_file, include_optimizer=False)
-    return model 
+    tf.keras.models.save_model(unpruned_model, keras_file, include_optimizer=False)
+    return unpruned_model
 
     
 def main(argv=None):
     vocab, dataset  = preprocess()
-    train(dataset, GRU_model, vocab)
+    unpruned_model = train(LSTM_model, dataset, vocab)
 if __name__ == '__main__':
     tf.app.run()
